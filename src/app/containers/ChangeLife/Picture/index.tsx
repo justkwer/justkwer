5c16f18df4ceaf931a5829031de27af3012671cf
Alex <justkwer@gmail.com>
2024-10-09 23:13:11 +0300

    fix(*): update resume (PDF)

--- src/app/containers/ChangeLife/Picture/index.tsx
+++ src/app/containers/ChangeLife/Picture/index.tsx
@@ -15,7 +15,7 @@ export const Picture = () => {
       <figcaption>
         <S.Title>{IS_REAL}</S.Title>
         <S.Subtitle>
-          <a href="/Alex-Yakovlev.pdf" download="Alex Yakovlev.pdf">
+          <a href="/Alex-Moore.en.pdf" download="Alex Moore.pdf">
             {CLICK}
           </a>
           &ensp;{TO_DOWNLOAD_OR}&ensp;
